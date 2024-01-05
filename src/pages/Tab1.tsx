ddbad3c30f22a3087eaa9570c2bb02384445368a
Tyler Abbott <tabbott85@gmail.com>
2024-01-04 23:10:08 -0600

    feat: player logic (wip)

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,46 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Tab1.css";
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>Playlists</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
+            <IonTitle size="large">Playlists</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <ExploreContainer name="Playlists Page">
+
+          <IonCard  routerLink="/editor/1">
+            <IonCardHeader>
+              <IonCardTitle>New Year's Eve 2023</IonCardTitle>
+              <IonCardSubtitle>60 Tracks</IonCardSubtitle>
+            </IonCardHeader>
+
+            <IonCardContent>
+              Memes, Music and More!
+            </IonCardContent>
+          </IonCard>
+        
+        </ExploreContainer>
       </IonContent>
     </IonPage>
   );
