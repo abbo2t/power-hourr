6734b6a45a1d02e103011edfbcf091ec9d59a0c9
Tyler Abbott <tabbott85@gmail.com>
2024-01-23 22:35:49 -0600

    Updates

--- assets/md.transition-legacy-eAzN0VK_.js
+++ assets/md.transition-legacy-eAzN0VK_.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-4xBuxLlR.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-jVd10HsZ.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
