f982797afea24fec825cfeb64ed69dbfe5ced4bd
Tyler Abbott <tabbott85@gmail.com>
2024-01-26 20:07:47 -0600

    Updates

--- assets/status-tap-legacy-vdxEjGnY.js
+++ assets/status-tap-legacy-vdxEjGnY.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-kcZMDovr.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-ziCB3Qu8.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
